a2ca704848cec0f32c43fc69f97798f384666057
OscarUrielCZ <oscar2410.oucz@gmail.com>
2025-03-05 18:15:36 -0600

    feature: inversión separado de los egresos

--- src/components/ResumeExpenses/index.tsx
+++ src/components/ResumeExpenses/index.tsx
@@ -5,16 +5,23 @@ import { numberWithCommas } from "../../common/utils.ts";
 import "./ResumeExpenses.css";
 
 function ResumeExpenses({ expenses }) {
-  let totalAmount: number = expenses.reduce(
-    (accumulator, currentValue) => accumulator + Number(currentValue.amount),
-    0
-  );
+  let expenseQuantity: number = 0;
+  let investmentQuantity: number = 0;
   let expensesCount: number = expenses.length;
+
+  expenses.forEach((expense) => {
+    if (expense.category === "Inversión")
+      investmentQuantity += Number(expense.amount);
+    else expenseQuantity += Number(expense.amount);
+  });
 
   return (
     <div className="container">
       <span className="stat-sentence">
-        Egresos totales: ${numberWithCommas(totalAmount)}
+        Egresos totales: ${numberWithCommas(expenseQuantity)}
+      </span>
+      <span className="stat-sentence">
+        Inversión de ${numberWithCommas(investmentQuantity)}
       </span>
       <span className="secondary-stat-sentence">
         {expensesCount} movimientos
